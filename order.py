4570d39052cacedae3f59f0d42052ba5f565adf2
Fastfoot0529 <144537485+Fastfoot0529@users.noreply.github.com>
2023-09-21 13:07:28 +0900

    Update order.py

--- order.py
+++ order.py
@@ -4,4 +4,4 @@ access = "8e3DU61NdWCwQfMZirxEjKRz6fD9LnwwxrXLWONd"          # 본인 값으로 
 secret = "xka5VvVeEZ8knUgKC4AlXIfq3SuvBLuwDgs1GifQ"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
-upbit.buy_market_order("KRW-BTC", "but", 10000,1)
+upbit.buy_market_order("KRW-BTC", 10000)
