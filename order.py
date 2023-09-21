3e4e15c1339a2c81033ceea28f652797cf96c2bf
Fastfoot0529 <144537485+Fastfoot0529@users.noreply.github.com>
2023-09-21 13:01:14 +0900

    Add files via upload

---
+++ order.py
@@ -0,0 +1,7 @@
+import pyupbit
+
+access = "8e3DU61NdWCwQfMZirxEjKRz6fD9LnwwxrXLWONd"          # 본인 값으로 변경
+secret = "xka5VvVeEZ8knUgKC4AlXIfq3SuvBLuwDgs1GifQ"          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+upbit.buy_market_order("KRW-BTC", "but", 10000,1)
